474a0877b2d043a454577efc860b48af305b35a1
techlojutt <muhammadshoaib2140jutt@gmail.com>
2024-09-04 16:50:53 +0500

    activity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import MsgBox from "./MsgBox.js";
 import ProductTab from "./ProductTab.js"
+
 
 // if we are not want to create an extra node of div then we use react fragment  <></>  
 function App() {
+   
   return (
     <div className = "App">
+      <MsgBox textColor = "yellow" userName = "shoaib" />
+      <MsgBox textColor = "green" userName = "bilal" />
+      <MsgBox textColor = "blue" userName = "ismail" />
+      <MsgBox textColor = "aqua" userName = "ibrahim" />
       <ProductTab/>
     </div>
   )
